e12c32f529aa7c940a88a5cc860d2a1cf62ab9df
Arthur Mello Pimentel <Arthurmellopimentel76@gmail.com>
2024-06-23 19:57:06 -0300

    website

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!-- footer -->
+<footer>
+    <div class="footerTeamLinks">
+      <a href=""><img src="./headerNav/headerNavImg/Logo - Saturno.png" alt=""></a>
+      <span class="footerBar"></span>
+      <div class="footerInformations">
+        <p>GITHUB</p>
+        <p>GITHUB</p>
+        <p>GITHUB</p>
+        <p>GITHUB</p>
+      </div>
+    </div>
+    <span class="footerSenacDiv">
+      <img class="footerSenac" src="./Footer/footerImgs/senac.png" alt="">
+    </span>
+  </footer>
+<!-- footer - End -->
